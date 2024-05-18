2c1642813bf62424970b6d4cb65d7dce4a41263d
enriquefnt <enriquefinetti@gmail.com>
2024-05-18 07:18:34 -0300

    nada

--- templates/listacasos.html.php
+++ templates/listacasos.html.php
@@ -24,9 +24,11 @@
     <td><?= $caso['localidad']; ?></td>
     
        <td class="text-center">
-        <a href="/ficha/print?idficha=<?= $caso['idficha'] ?>">
-          <i class="bi bi-pencil-square"></i>
-        </a>
+        <!-- <a href="/ficha/print?idficha=<?= $caso['idficha'] ?>" target="_blank"><i class="fa-regular fa-file-pdf"></i> -->
+        <a href="/ficha/print?idficha=<?= $caso['idficha'] ?>&nombre=<?= urlencode($caso['nombre']) ?>" target="_blank"><i class="fa-regular fa-file-pdf"></i></a>
+
+       </a>
+      
       </td> 
    
   </tr>
