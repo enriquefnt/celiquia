e7bb945ad79bddaf10073006aa78dd3275a57278
enriquefnt <enriquefinetti@gmail.com>
2024-05-22 15:22:21 -0300

    ajustes menores

--- templates/home.html.php
+++ templates/home.html.php
@@ -1,86 +1,83 @@
-<?php $title="Anuncios" ;?>
+<?php $title = "Anuncios"; ?>
 <main>
-<section class="d-flex justify-content-center justify-content-lg-between "> 
-  
- <div class="conteiner container-sm"> 
-  
- <!-- <img src="https://celiaquia.v.je/cueto.jpg" class="img-fluid mx-auto" alt="Cueto"> -->
- <img src="https://celiaquia.v.je/cueto.jpg" class="img-fluid mx-auto" alt="Cueto" style="width:800px; height: 500px; object-fit: contain;">
-   </div> 
- </section>
- 
+  <section class="d-flex justify-content-center justify-content-lg-between ">
+
+    <div class="conteiner container-sm">
+      <img src="https://celiaquia.v.je/cueto.jpg" class="img-fluid mx-auto" alt="Cueto" style="width:800px; height: 500px; object-fit: contain;">
+    </div>
+  </section>
+
 </main>
 
- <footer class="text-center text-lg-start bg-white text-muted">
-  
+<footer class="text-center text-lg-start bg-white text-muted">
 
- 
+
+
   <section class="">
     <div class="container text-center text-md-start mt-5">
-            <div class="row mt-3 pt-1">
-        
+      <div class="row mt-3 pt-1">
+
         <div class="col-md-3 col-lg-4 col-sm-3 mx-auto mb-4">
-          
-          <h6 class="text-uppercase fw-bold mb-4" >
+
+          <h6 class="text-uppercase fw-bold mb-4">
             <i class="fas fa-gem me-3 text-primary"></i>Institución
           </h6>
           <p>
-          Ministerio de Salud Pública de Salta.
+            Ministerio de Salud Pública de Salta.
           </p>
-          <p >
+          <p>
             Subsecretaría de Medicina Social
           </p>
-          <p >
+          <p>
             Direccion de Nutrición y Alimentación Saludable
           </p>
         </div>
         <div class="col-md-3 col-lg-4 col-sm-3 mx-auto mb-4">
-         
-          <h6 class="text-uppercase fw-bold mb-4" >
+
+          <h6 class="text-uppercase fw-bold mb-4">
             <i class="fas fa-gem me-3 text-primary"></i>Responsabble del Programa de Celiquía:
           </h6>
-          <p >
-            Lic. Silvia Quipildor (Jefa)  </p> <p > 
+          <p>
+            Lic. Silvia Quipildor (Jefa) </p>
+          <p>
           </p>
         </div>
-        
+
         <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
           <h6 class="text-uppercase fw-bold mb-4"><i class="fas fa-gem me-3 text-primary"></i>Contacto</h6>
           <p><i class="fas fa-home me-3 text-secondary"></i> Belgrano 1349 -1º piso - Salta Capital - CP 4400</p>
-          <p><i class="fas fa-envelope me-3 text-secondary"></i>programaceliquia@gmail.com </p>
-          <p><i class="fas fa-phone me-3 text-secondary"></i> 0387-4370128/127 <br> &nbsp &nbsp &nbsp &nbsp  0387-152458570</p>
+          <p><i class="fas fa-envelope me-3 text-secondary"></i>programaceliaquia@gmail.com </p>
+          <p><i class="fas fa-phone me-3 text-secondary"></i> 0387-4370128/127 <br> &nbsp &nbsp &nbsp &nbsp 0387-152458570</p>
           <p><i class="fas fa-print me-3 text-secondary"></i> 0387-4320695</p>
         </div>
-      
+
       </div>
-     
+
     </div>
   </section>
-  
+
   <section class="d-flex justify-content-center justify-content-sm-between p-4 border-bottom">
-    
+
     <div class="me-5 d-none d-lg-block">
       <span><b>Nuestras redes sociales:</b></span>
     </div>
-    
 
-   
+
+
     <div>
-      <a href="https://www.facebook.com/direcnutyasaludable/"  target="_blank" class="me-4 link-secondary">
+      <a href="https://www.facebook.com/direcnutyasaludable/" target="_blank" class="me-4 link-secondary">
         <i class="fab fa-facebook-f"></i>
       </a>
       <a href="https://twitter.com/SNyAS3" target="_blank" class="me-4 link-secondary">
         <i class="fab fa-twitter"></i>
       </a>
-         </div>
-   
+    </div>
+
   </section>
 
-  <section class="d-flex justify-content-center justify-content-lg-between "> 
- <div class="conteiner container-sm"> 
-  
-   </div> 
- </section>
-</footer> 
+  <section class="d-flex justify-content-center justify-content-lg-between ">
+    <div class="conteiner container-sm">
 
-
+    </div>
+  </section>
+</footer>
