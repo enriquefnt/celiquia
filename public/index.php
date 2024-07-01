b673fb3929334bcd076cbf551054695ff0c8a2c7
enriquefnt <enriquefinetti@gmail.com>
2024-07-01 13:05:44 -0300

    nada

--- public/index.php
+++ public/index.php
@@ -1,5 +1,4 @@
 <?php
-
 // Configuración de cabeceras de seguridad
 header('X-Frame-Options: SAMEORIGIN');
 header("Content-Security-Policy: default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://code.jquery.com https://cdnjs.cloudflare.com https://cdn.jsdelivr.net https://kit.fontawesome.com https://cdn.datatables.net; style-src 'self' 'unsafe-inline' https://cdnjs.cloudflare.com https://cdn.jsdelivr.net https://fonts.googleapis.com https://cdn.datatables.net https://ka-f.fontawesome.com; font-src 'self' https://fonts.gstatic.com https://cdnjs.cloudflare.com https://kit.fontawesome.com https://cdn.datatables.net https://ka-f.fontawesome.com https://cdn.jsdelivr.net; connect-src 'self' https://ka-f.fontawesome.com; img-src 'self' data:; object-src 'none'; frame-ancestors 'none'; base-uri 'self'; form-action 'self';");
@@ -9,7 +8,7 @@ if (session_status() == PHP_SESSION_NONE) {
     session_set_cookie_params([
         'lifetime' => 21600, // 6 horas de expiración
         'path' => '/',
-      //  'domain' => 'celiquia.v.je',
+      // 'domain' => '.celiaquia.v.je',
         'secure' => true, // Asegúrate de que la cookie solo se envíe a través de HTTPS
         'httponly' => true, // Marca HttpOnly
         'samesite' => 'Strict' // Configura el atributo SameSite
